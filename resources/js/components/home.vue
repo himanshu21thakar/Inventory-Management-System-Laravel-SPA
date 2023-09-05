bd307834738a401a193e0d841e8847aee8bb8833
himanshu.thakar <himanshu21thakar@gmail.com>
2023-09-05 21:06:49 +0530

    cc

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -15,12 +15,11 @@
 					<div class="card-body">
 						<div class="row align-items-center">
 							<div class="col mr-2">
-								<div class="text-xs font-weight-bold text-uppercase mb-1">Today Income</div>
-								<!-- Use the computed property for formatting -->
-								<div class="h5 mb-0 font-weight-bold text-gray-800">₹{{ formattedIncome }}</div>
+								<div class="text-xs font-weight-bold text-uppercase mb-1">Today Sell Amount</div>
+								<div class="h5 mb-0 font-weight-bold text-gray-800">${{ todaySells }}</div>
 								<div class="mt-2 mb-0 text-muted text-xs">
-									<span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
-									<span>Since last years</span>
+									<span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+									<span>Since last month</span>
 								</div>
 							</div>
 							<div class="col-auto">
@@ -37,8 +36,7 @@
 						<div class="row no-gutters align-items-center">
 							<div class="col mr-2">
 								<div class="text-xs font-weight-bold text-uppercase mb-1">Today Income</div>
-								<!-- Use the computed property for formatting -->
-								<div class="h5 mb-0 font-weight-bold text-gray-800">₹{{ formattedIncome }}</div>
+								<div class="h5 mb-0 font-weight-bold text-gray-800">${{ income }}</div>
 								<div class="mt-2 mb-0 text-muted text-xs">
 									<span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
 									<span>Since last years</span>
@@ -57,12 +55,11 @@
 					<div class="card-body">
 						<div class="row no-gutters align-items-center">
 							<div class="col mr-2">
-								<div class="text-xs font-weight-bold text-uppercase mb-1">Today Income</div>
-								<!-- Use the computed property for formatting -->
-								<div class="h5 mb-0 font-weight-bold text-gray-800">₹{{ formattedIncome }}</div>
+								<div class="text-xs font-weight-bold text-uppercase mb-1">Today Due</div>
+								<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">${{ due }}</div>
 								<div class="mt-2 mb-0 text-muted text-xs">
-									<span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
-									<span>Since last years</span>
+									<span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20.4%</span>
+									<span>Since last month</span>
 								</div>
 							</div>
 							<div class="col-auto">
@@ -78,12 +75,11 @@
 					<div class="card-body">
 						<div class="row no-gutters align-items-center">
 							<div class="col mr-2">
-								<div class="text-xs font-weight-bold text-uppercase mb-1">Today Income</div>
-								<!-- Use the computed property for formatting -->
-								<div class="h5 mb-0 font-weight-bold text-gray-800">₹{{ formattedIncome }}</div>
+								<div class="text-xs font-weight-bold text-uppercase mb-1">Expense Amount</div>
+								<div class="h5 mb-0 font-weight-bold text-gray-800">${{ expense }}</div>
 								<div class="mt-2 mb-0 text-muted text-xs">
-									<span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
-									<span>Since last years</span>
+									<span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 1.10%</span>
+									<span>Since yesterday</span>
 								</div>
 							</div>
 							<div class="col-auto">
@@ -95,49 +91,46 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-lg-12 mb-4">
-				<!-- Simple Tables -->
-				<div class="card">
-					<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-						<h2 class="m-0 font-weight-bold text-primary">Stock Out Products</h2>
-						<router-link to="/store-product" class="btn btn-primary float-right"
-							style="margin-top: 6px;margin-right: 6px;">Add Product</router-link>
+				<div class="col-lg-12 mb-4">
+					<!-- Simple Tables -->
+					<div class="card">
+						<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+							<h2 class="m-0 font-weight-bold text-primary">Stock Out Products</h2>
+							<router-link to="/store-product" class="btn btn-primary float-right" style="margin-top: 6px;margin-right: 6px;">Add Product</router-link>
+						</div>
+						<div class="table-responsive">
+							<table class="table align-items-center table-flush">
+								<thead class="thead-light">
+									<tr>
+										<th>Product Name</th>
+										<th>Product Code</th>
+										<th>Image</th>
+										<th>Buying Price</th>
+										<th>Status</th>
+										<th>Quantity</th>
+										<th>Action</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr v-for="product in stockOutProducts" :key="product.id">
+										<td>{{ product.product_name }}</td>
+										<td>{{ product.product_code }}</td>
+										<td><img :src="product.image" id="img_size"></td>
+										<td>{{ product.buying_price }}</td>
+										<td v-if="product.product_quantity >= 1"><span class="badge badge-success">Available</span></td>
+										<td v-else=""><span class="badge badge-danger">Stock Out</span></td>
+										<td>{{ product.product_quantity }}</td>
+										<td>
+											<router-link :to="{name: 'editStock', params: {id: product.id}}" class="btn btn-sm btn-primary">Edit Stock</router-link>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<div class="card-footer"></div>
 					</div>
-					<div class="table-responsive">
-						<table class="table align-items-center table-flush">
-							<thead class="thead-light">
-								<tr>
-									<th>Product Name</th>
-									<th>Product Code</th>
-									<th>Image</th>
-									<th>Buying Price</th>
-									<th>Status</th>
-									<th>Quantity</th>
-									<th>Action</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr v-for="product in stockOutProducts" :key="product.id">
-									<td>{{ product.product_name }}</td>
-									<td>{{ product.product_code }}</td>
-									<td><img :src="product.image" id="img_size"></td>
-									<td>{{ product.buying_price }}</td>
-									<td v-if="product.product_quantity >= 1"><span
-											class="badge badge-success">Available</span></td>
-									<td v-else=""><span class="badge badge-danger">Stock Out</span></td>
-									<td>{{ product.product_quantity }}</td>
-									<td>
-										<router-link :to="{ name: 'editStock', params: { id: product.id } }"
-											class="btn btn-sm btn-primary">Edit Stock</router-link>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-					<div class="card-footer"></div>
 				</div>
 			</div>
-		</div>
 	</div>
 </template>
 
@@ -146,20 +139,13 @@ export default {
 
 	name: 'home',
 
-	created() {
+	created(){
 		if (!User.loggedIn()) {
-			this.$router.push({ name: '/' })
+			this.$router.push({name: '/'})
 		}
 	},
 
-	computed: {
-		// Computed property to format income with ₹ sign
-		formattedIncome() {
-			return `₹${this.income}`;
-		},
-	},
-
-	data() {
+	data () {
 		return {
 			todaySells: '',
 			income: '',
@@ -169,7 +155,7 @@ export default {
 		}
 	},
 
-	mounted() {
+	mounted(){
 		this.todaySell();
 		this.todayIncome();
 		this.todayDue();
@@ -178,37 +164,37 @@ export default {
 	},
 
 	methods: {
-		todaySell() {
+		todaySell(){
 			axios.get('/api/today/sell')
-				.then(({ data }) => (this.todaySells = data))
-				.catch()
+			.then(({data}) => (this.todaySells = data))
+			.catch()
 		},
-		todayIncome() {
+		todayIncome(){
 			axios.get('/api/today/income')
-				.then(({ data }) => (this.income = data))
-				.catch()
+			.then(({data}) => (this.income = data))
+			.catch()
 		},
-		todayDue() {
+		todayDue(){
 			axios.get('/api/today/due')
-				.then(({ data }) => (this.due = data))
-				.catch()
+			.then(({data}) => (this.due = data))
+			.catch()
 		},
-		expenses() {
+		expenses(){
 			axios.get('/api/total/expense')
-				.then(({ data }) => (this.expense = data))
-				.catch()
+			.then(({data}) => (this.expense = data))
+			.catch()
 		},
-		stockOutProduct() {
+		stockOutProduct(){
 			axios.get('/api/stockout/product')
-				.then(({ data }) => (this.stockOutProducts = data))
-				.catch()
+			.then(({data}) => (this.stockOutProducts = data))
+			.catch()
 		},
 	}
 }
 </script>
 
 <style lang="css" scoped>
-#img_size {
+#img_size{
 	width: 40px;
 }
 </style>
